e25f12246e9b8747cfa806014ca0229f0df82d50
VampireNeo <neo.choi@gmail.com>
2013-06-19 02:19:33 +0800

    start adding url for pt

--- PassionTimes/src/main/java/com/vampireneoapps/passiontimes/MainActivity.java
+++ PassionTimes/src/main/java/com/vampireneoapps/passiontimes/MainActivity.java
@@ -1,9 +1,22 @@
 package com.vampireneoapps.passiontimes;
 
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
 
 public class MainActivity extends SherlockActivity {
 
@@ -13,6 +26,11 @@ public class MainActivity extends SherlockActivity {
         setTheme(R.style.Theme_Sherlock_Light); //Used for theme switching in samples
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        boolean isonline = isOnline(this);
+        if (isonline) {
+            //new DownloadModelList().execute("http://test.crown-motors.com/model.js");
+        }
     }
 
 
@@ -28,5 +46,61 @@ public class MainActivity extends SherlockActivity {
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
         return true;
     }
-    
+
+    private class DownloadModelList extends AsyncTask<String, Void, ArrayList<VehicleObject>> {
+        @Override
+        protected ArrayList<VehicleObject> doInBackground(String... params) {
+            try {
+                return downloadUrl((String)params[0]);
+            } catch (IOException e) {
+                //return "Unable to retrieve web page. URL may be invalid.";
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<VehicleObject> l) {
+            AddVehicleFromList(l);
+            ((ModelListFragment) getSupportFragmentManager().findFragmentById(R.id.model_list)).updateList();
+        }
+
+        private ArrayList<VehicleObject> downloadUrl(String myurl) throws IOException {
+            InputStream is = null;
+            FileOutputStream fos = null;
+
+            try {
+                URL url = new URL(myurl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.connect();
+                is = conn.getInputStream();
+                byte[] buffer = new byte[4096];
+                fos = openFileOutput(DATA_FILE_NAME, Context.MODE_PRIVATE);
+                int n = is.read(buffer, 0, buffer.length);
+                while (n >= 0) {
+                    fos.write(buffer, 0, n);
+                    n = is.read(buffer, 0, buffer.length);
+                }
+                FileInputStream inputStream = openFileInput(DATA_FILE_NAME);
+                return AddVehicleFromInputStream(inputStream);
+            }
+            finally {
+                if (is != null) {
+                    is.close();
+                }
+                if (fos != null) {
+                    fos.close();
+                }
+            }
+        }
+    }
+
+    private boolean isOnline(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return (netInfo != null && netInfo.isConnectedOrConnecting());
+    }
 }
